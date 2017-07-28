45f1d547c22ac91229777476ee00fcdc3a993b56
david <druizcmx@gmail.com>
2017-07-28 15:34:06 -0500

    commit inicial

---
+++ src/main/java/com/citi/api/widget/WidgetServiceApplication.java
@@ -0,0 +1,12 @@
+package com.citi.api.widget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetServiceApplication.class, args);
+	}
+}
